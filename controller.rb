0fe93bb007fe61a09297d7c6eda79a78ee6419b3
Campbell Miller <campbellmiller4e@gmail.com>
2018-06-04 18:53:53 +0100

    adds stylesheet

--- controller.rb
+++ controller.rb
@@ -7,4 +7,13 @@ also_reload('models/*')
 get '/RPS/:hand1/:hand2' do
   rockpaperscissors = RockPaperScissors.new(params[:hand1], params[:hand2])
   @rockpaperscissors = rockpaperscissors.game()
+  erb( :result )
+end
+
+get '/' do
+  erb( :home )
+end
+
+get '/THE GAME' do
+  erb( :game )
 end
